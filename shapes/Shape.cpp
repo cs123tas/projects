8a57f63d0a0a7d46c52c2dfddc24623e4ad311dc
Dylan Tian <dylantian@fb.com>
2020-09-28 22:07:37 -0400

    fix inline

--- shapes/Shape.cpp
+++ shapes/Shape.cpp
@@ -6,7 +6,7 @@
 
 using namespace CS123::GL;
 
-inline void insertVec3(std::vector<float> &data, glm::vec3 v){
+void insertVec3(std::vector<float> &data, glm::vec3 v){
     data.push_back(v.x);
     data.push_back(v.y);
     data.push_back(v.z);
